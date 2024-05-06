e3cdcb980bf45bf84c394dd04bb9d946db2f4038
anasmunawar2 <anasmunawar2@gmail.com>
2024-05-06 21:23:36 +0500

    chat-component-added

---
+++ src/components/chat/Chat.jsx
@@ -0,0 +1,26 @@
+import "./chat.css";
+
+const Chat = () => {
+  return (
+    <div className="chat">
+      <div className="top">
+        <div className="user">
+          <img src="./avatar.png" alt="" />
+          <div className="texts">
+            <span>John Doe</span>
+            <p>Lorem Ipsum</p>
+          </div>
+        </div>
+        <div className="icons">
+          <img src="./phone.png" alt="" />
+          <img src="./video.png" alt="" />
+          <img src="./info.png" alt="" />
+        </div>
+      </div>
+      <div className="center"></div>
+      <div className="bottom"></div>
+    </div>
+  );
+};
+
+export default Chat;
