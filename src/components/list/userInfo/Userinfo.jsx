2a35d41da1403ce654bc45811764d31a6d50dd28
anasmunawar2 <anasmunawar2@gmail.com>
2024-05-10 02:11:45 +0500

    zustand-integrated

--- src/components/list/userInfo/Userinfo.jsx
+++ src/components/list/userInfo/Userinfo.jsx
@@ -1,11 +1,13 @@
 import "./userInfo.css";
+import { useUserStore } from "./../../../lib/userStore";
 
 const Userinfo = () => {
+  const { currentUser } = useUserStore();
   return (
     <div className="userInfo">
       <div className="user">
-        <img src="./avatar.png" alt="" />
-        <h5>John Doe</h5>
+        <img src={currentUser.avatar || "./avatar.png"} alt="" />
+        <h5>{currentUser.username}</h5>
       </div>
       <div className="icons">
         <img src="./more.png" alt="" />
